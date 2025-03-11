978287ecdd2ba2fe9bb7c71bd484b6284cd9cd4c
Uriya Carlebach <uriya48@gmail.com>
2025-03-11 20:53:49 +0200

    add print in programmer 2 "popi.lop"

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ student = Student("Gilad", 25, "Agricalture", 1 ,91)
 print("This from programmer 1, hi how are you, 123")
 # student.foo()
 
-print("This is from programmer 2, test conflict")
+print("This is from programmer 2, popi.lop")
 employee = Employee("John", 40, "Software Engineer", 45000)
 people = [student, employee]
 for person in people:
