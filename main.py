d8784fd08d2253ae86a3454afef2cdfa2dcb0122
Uriya Carlebach <uriya48@gmail.com>
2025-03-11 17:27:52 +0200

    fixed age, not update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Student import Student
 from Employee import Employee
 
-student = Student("Gilad", 22, "Agricalture", 1 ,91)
+student = Student("Gilad", 25, "Agricalture", 1 ,91)
 # student.foo()
 
 employee = Employee("John", 40, "Software Engineer", 45000)
